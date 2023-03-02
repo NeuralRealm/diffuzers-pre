09ac7c33bfb3e43bd43b78cc0f981c3c18ab5505
Kumar Saksham <everydaycodings@gmail.com>
2023-03-02 21:22:04 +0530

    droped the idea for ckpt

--- stablefusion/models/realesrgan/inference_realesrgan.py
+++ stablefusion/models/realesrgan/inference_realesrgan.py
@@ -142,7 +142,10 @@ def main(model_name, denoise_strength, tile, tile_pad, pre_pad, fp32, gpu_id, fa
         
         img = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
         st.image(img)
-        
+        try:
+            utils.display_and_download_images(output_images=Image.open(output), metadata="None")
+        except:
+            utils.display_and_download_images(output_images=img, metadata="None")
         try:
             utils.display_and_download_images(output_images=np.array(img), metadata="None")
         except:
