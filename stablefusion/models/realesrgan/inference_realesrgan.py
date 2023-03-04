e7b6464eeabd1079ed7518ecf0bffc104e5d492a
Kumar Saksham <everydaycodings@gmail.com>
2023-03-04 19:10:20 +0530

    droped the idea for ckpt

--- stablefusion/models/realesrgan/inference_realesrgan.py
+++ stablefusion/models/realesrgan/inference_realesrgan.py
@@ -13,6 +13,8 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 import base64
+import datetime
+
 
 def main(model_name, denoise_strength, tile, tile_pad, pre_pad, fp32, gpu_id, face_enhance, outscale, input_image, model_path):
     # determine models according to model names
@@ -102,14 +104,14 @@ def main(model_name, denoise_strength, tile, tile_pad, pre_pad, fp32, gpu_id, fa
         img = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
         st.image(img)
 
-        try:
-            output_img = Image.fromarray(img)
-        except:
-            output_img = Image.fromarray(output, mode="RGB")
+
+        output_img = Image.fromarray(img)
         buffered = BytesIO()
         output_img.save(buffered, format="PNG")
         img_str = base64.b64encode(buffered.getvalue()).decode()
-        href = f'<a href="data:file/png;base64,{img_str}" download="gfpgan.png">Download Image</a>'
+        now = datetime.datetime.now()
+        formatted_date_time = now.strftime("%Y-%m-%d_%H_%M_%S")
+        href = f'<a href="data:file/png;base64,{img_str}" download="realesrgan{formatted_date_time}.png">Download Image</a>'
         st.markdown(href, unsafe_allow_html=True)
         
             
