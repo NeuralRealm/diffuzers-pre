5bb70912451fcfbe39a5115d2daa373a6444022d
Kumar Saksham <everydaycodings@gmail.com>
2023-03-07 20:45:44 +0530

    added encoding utf-8 in setup.py

---
+++ stablefusion/pages/9_Diffusion_Mixture.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+from stablefusion import utils
+from stablefusion.scripts.pose_editor import OpenPoseEditor
+from stablefusion.Home import read_model_list
+
+
+def app():
+    utils.create_base_page()
+    with st.form("openpose_editor_form"):
+        model = st.selectbox(
+            "Which model do you want to use for OpenPose?",
+            options=read_model_list()
+        )
+        submit = st.form_submit_button("Load model")
+    if submit:
+        st.session_state.difusion_mixture = model
+        with st.spinner("Loading model..."):
+            diffusion_mixture = OpenPoseEditor(
+                model=model,
+                device=st.session_state.device,
+                output_path=st.session_state.output_path,
+            )
+            st.session_state.difusion_mixture = diffusion_mixture
+    if "openpose_editor" in st.session_state:
+        st.write(f"Current model: {st.session_state.difusion_mixture}")
+        st.session_state.difusion_mixture.app()
+
+
+if __name__ == "__main__":
+    app()
